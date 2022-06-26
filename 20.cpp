a6259b81477cbdd1f18f1bb5fc9af9db748c130f
Juan Miguel <juanmiguelruizsupe@gmail.com>
2022-06-26 13:31:26 -0500

    Iterador sobre la entrada

--- 20.cpp
+++ 20.cpp
@@ -4,31 +4,35 @@
 using namespace std;
 int main()
 {
-  char letra;
+  char *letra;
   cout << "ingresa una vocal del alfabeto" << endl;
   cin >> letra;
-  if ((letra == 'a') || (letra == 'A'))
+  while (letra != "exit")
   {
-    cout << "es una vocal" << endl;
+    if ((letra == "a") || (letra == "A"))
+    {
+      cout << "es una vocal" << endl;
+    }
+    else if ((letra == "e") || (letra == "E"))
+    {
+      cout << "es una vocal" << endl;
+    }
+    else if ((letra == "i") || (letra == "I"))
+    {
+      cout << "es una vocal" << endl;
+    }
+    else if ((letra == "o") || (letra == "O"))
+    {
+      cout << "es una vocal" << endl;
+    }
+    else if ((letra == "u") || (letra == "U"))
+    {
+      cout << "es una vocal" << endl;
+    }
+    else
+    {
+      cout << "no es una vocal" << endl;
+    }
   }
-  else if ((letra == 'e') || (letra == 'E'))
-  {
-    cout << "es una vocal" << endl;
-  }
-  else if ((letra == 'i') || (letra == 'I'))
-  {
-    cout << "es una vocal" << endl;
-  }
-  else if ((letra == 'o') || (letra == 'O'))
-  {
-    cout << "es una vocal" << endl;
-  }
-  else if ((letra == 'u') || (letra == 'U'))
-  {
-    cout << "es una vocal" << endl;
-  }
-  else
-  {
-    cout << "no es una vocal" << endl;
-  }
+  cout << "Fin del programa";
 }
